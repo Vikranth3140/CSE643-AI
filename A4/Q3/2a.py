647bb4b2b4834bbd36bd4d8ff791f1d96a8d1c44
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-12-02 08:50:15 +0530

    Q3/2a Done

--- A4/Q3/2a.py
+++ A4/Q3/2a.py
@@ -1,24 +1,21 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 import os
 
 os.makedirs("Plots", exist_ok=True)
 
-# train_data = pd.read_csv('../dataset/train.csv')
-train_data = pd.read_csv('../processed_train_data.csv')
+train_data = pd.read_csv('../Q2/X_train_final_with_categories.csv')
 
-X = train_data.drop(columns=['Price'])
-y = train_data['Price']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train = train_data.drop(columns=['Price', 'Price_Category'])
+y_train = train_data['Price']
 
 # Training Decision Tree on processed data
 model = DecisionTreeRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 # Extract feature importances from the trained Decision Tree model
-feature_importances = pd.Series(model.feature_importances_, index=X.columns)
+feature_importances = pd.Series(model.feature_importances_, index=X_train.columns)
 
 plt.figure(figsize=(10, 6))
 feature_importances.sort_values(ascending=False).plot(kind='bar', color='skyblue', edgecolor='black')
